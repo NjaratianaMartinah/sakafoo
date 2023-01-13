89718e0f662afedbf9f6918f1591f015ceae2f22
Njaratiana <njaratiana.rahalinjanahary@pulse.mg>
2023-01-13 08:33:48 +0300

    init project

---
+++ src/test/java/com/example/sakafoo/SakafooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sakafoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakafooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
